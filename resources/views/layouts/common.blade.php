5231cef363b62a14bada40ae2dda5902848e9ca7
udaikue <ikueikue@gmail.com>
2022-11-20 22:06:24 +0900

    Add buttons to index page

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -8,18 +8,51 @@
                 font-family: 'Nunito', sans-serif;
                 font-size: 14pt;
                 color: #999;
-                margin: 5px;;
+                text-align: center;
+                margin: 5px;
             }
             h1 {
                 font-size: 20pt;
                 color: #666;
                 margin: 10px;
             }
-            th {
+            th, td {
               font-size: 14pt;
+              padding: 0.25em;
             }
             .content {
+                display: inline-block;
                 margin: 10px;
+            }
+            .add-btn {
+                display: inline-block;
+                text-decoration: none;
+                background: #87befd;
+                color: #FFF;
+                width: 60px;
+                height: 60px;
+                line-height: 60px;
+                border-radius: 50%;
+                text-align: center;
+                overflow: hidden;
+                transition: .4s;
+                }
+            .add-btn:hover {
+                background: #668ad8;
+                }
+            .table-btn{
+                display: inline-block;
+                padding: 0.5em 1em;
+                text-decoration: none;
+                color: #67c5ff;
+                border: double 4px #67c5ff;
+                border-radius: 3px;
+                transition: .4s;
+              }
+
+            .table-btn:hover {
+                background: #67c5ff;
+                color: white;
             }
             .footer {
                 text-align: left;
--- resources/views/tango/index.blade.php
+++ resources/views/tango/index.blade.php
@@ -3,12 +3,16 @@
 @section('title', '単語リスト')
 
 @section('content')
+    <a href="{{ route('tango.create') }}" class="add-btn">add</a>
+
     <table>
         @foreach ($tangos as $tango)
         <tr>
             <td>{{$tango->word}}</td>
             <td>{{$tango->memo}}</td>
             <td>{{$tango->done}}</td>
+            <td><a href="{{ route('tango.edit', $tango->id) }}" class="table-btn">done</a></td>
+            <td><a href="{{ route('tango.edit', $tango->id) }}" class="table-btn">edit</a></td>
         </tr>
         @endforeach
     </table>
